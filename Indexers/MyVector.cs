597f0c0c8ad40453d557bac70dd14f22667cc4a3
AnSantos99 <an.santos95@hotmail.com>
2021-12-07 17:10:11 +0000

    Indexers

---
+++ Indexers/MyVector.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Indexers
+{
+    public struct MyVector
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public MyVector(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public float this[int index]
+        {
+            get
+            {
+                if(index == 0) return X;
+                else if(index == 1) return Y;
+                throw new IndexOutOfRangeException();
+                
+            }
+
+            set
+            {
+                if(index == 0) X = value;
+                else if(index == 1) Y = value;
+                throw new IndexOutOfRangeException();
+                
+            }
+        }
+
+        public float this [string index]
+        {
+            get
+            {
+                index = index.ToLower();
+
+                if(index == "x" || index == "a" || index == "0") return X;
+                if(index == "y" || index == "b" || index == "1") return Y;
+                throw new ArgumentException();
+                
+            }
+        }
+    }
+}
---
+++ Indexers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyVector m1 = new MyVector(1.5f, 2);
+
+            MyVector v2 = new MyVector()
+            {
+                [0] = 1.3f,
+                [1] = -10
+            };
+
+            Console.WriteLine($"X= {m1[0]} | Y= {m1[1]}");
+
+            Console.WriteLine($"X= {v2["A"]} | Y= {v2["y"]}");
+
+
+
+            
+        }
+    }
+}
